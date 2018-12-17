87a5f2660a4859fc0d62b512d40e1e228720a18e
NOLOGY\Craig <craig@nology.io>
2018-12-17 09:32:51 +0000

    Init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PlatformDemo
+# Platform
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.7.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'platform-demo'`, () => {
+  it(`should have as title 'platform'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('platform-demo');
+    expect(app.title).toEqual('platform');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to platform-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to platform!');
   });
 });
